c4f49d760910c40311488cf9aa90621f5a2f13d4
林淡 <chenyuge.gcy@alibaba-inc.com>
2025-01-17 16:47:53 +0800

    删除无用代码

--- miniprogram/pages/test/index.ts
+++ miniprogram/pages/test/index.ts
@@ -1,8 +1,4 @@
-import Score from "../../utils/score"
 import { IScore } from "../../common/interface/score";
-import { beatNumPerSection, beatTime, musicInfo, scorePlayOrder, paragraphs } from "./data";
-
-const app = getApp()
 
 Page({
   data: {
@@ -10,15 +6,6 @@ Page({
   },
 
   onLoad: function () {
-    const myScore = new Score({
-      playOrder: scorePlayOrder,
-      beatTime: beatTime,
-      musicInfo: musicInfo,
-      beatNumPerSection: beatNumPerSection,
-      paragraphs: paragraphs
-    });
-    console.log('===== ~  myScore：', myScore.scores);
-    this.setData({scores: myScore.scores})
 
   },
 })
