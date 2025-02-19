0bd321137751e643332413ddff4b3564efd865f2
林淡 <chenyuge.gcy@alibaba-inc.com>
2025-02-19 21:01:54 +0800

    feat: 修改记录位置

--- miniprogram/pages/things/index.ts
+++ miniprogram/pages/things/index.ts
@@ -3,6 +3,7 @@ import { queryList, updateThing } from '../../utils/emas/things';
 Page({
   data: {
     list: [],
+    showMenuId: '',
   },
   onLoad() {
     this.loadData();
@@ -14,6 +15,25 @@ Page({
         list: res,
       });
     });
+  },
+  touchStart(e) {
+    this.touchStartX = e.changedTouches?.[0]?.clientX;
+  },
+  touchMove(e) {
+
+  },
+  touchEnd(e) {
+    const { id } = e.currentTarget.dataset;
+    this.touchEndX = e.changedTouches?.[0]?.clientX;
+    if (this.touchStartX && this.touchEndX) {
+      console.log('===== ~ touchEnd:', e);
+      if (this.touchStartX - this.touchEndX > 200) {
+        this.setData({ showMenuId: id })
+      } else if (this.touchEndX - this.touchStartX > 200) {
+        this.setData({ showMenuId: '' })
+
+      }
+    }
   },
   onUpdateOperationTime(e) {
     const { _id } = e.currentTarget.dataset;
