a104fe2717d1694d8fd79c635eec6aa3659a6a3c
林淡 <chenyuge.gcy@alibaba-inc.com>
2025-03-12 13:55:56 +0800

    优化首页样式

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,16 +4,22 @@ Page({
       {
         key: 'record',
         label: '陶笛录音',
+        icon: 'music',
+        color: "#FFCCCC",
         page: '/pages/record/index',
       },
       {
         key: 'things',
         label: '办事记录',
+        icon: 'list',
+        color: "#CCFFFF",
         page: '/pages/things/index',
       },
       {
         key: 'schulte-grid',
         label: '舒尔特方格',
+        icon: 'component-grid',
+        color: "#CCCCFF",
         page: '/pages/schulte-grid/index',
       }
     ],
